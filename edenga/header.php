7a054819b3816800ac53891ab197b4a3192e59e3
Tom Nolane <gotopredestenacia@gmail.com>
2017-11-22 16:36:31 +0300

    add new googlesyndication edenga.ru

--- edenga/header.php
+++ edenga/header.php
@@ -22,7 +22,14 @@
 <script src="/modules/html5shiv/html5shiv.js"></script>
 <![endif]-->
 <?php if ($this->uri->segment(1) != 'form') {
-    require 'templates/common/googlesyndication.php';
+	//require 'templates/common/googlesyndication.php';
+	echo '
+	<script>
+	  (adsbygoogle = window.adsbygoogle || []).push({
+		google_ad_client: "ca-pub-2044590115529379",
+		enable_page_level_ads: true
+	  });
+	</script>';
 }?>
 <script charset="UTF-8" src="//cdn.sendpulse.com/28edd3380a1c17cf65b137fe96516659/js/push/9e68a9b1b0fb51d74cc1b74eebc2e588_1.js" async></script>
 
