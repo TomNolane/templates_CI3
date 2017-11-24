2fd231029369d65c9075f7c2ae0c3628b8e61524
Tom Nolane <gotopredestenacia@gmail.com>
2017-11-24 19:16:18 +0300

    fix godzaim.ru

--- godzaim/header.php
+++ godzaim/header.php
@@ -1,27 +1,74 @@
+<?php  
+if(!isset($my_title)) 
+	$my_title = 'Срочные займы круглосуточно без проверок Онлайн';
+
+if(!isset($description))
+	$description = 'Godzaim - лучший онлайн сервис по выдаче мгновенных займов и кредитов без проверки вашей кредитной истории. Только у нас лучшие кредитные предложения!';
+?>
 <!DOCTYPE html>
-<html>
+<html lang="ru">
 <head>
-<title>Godzaim.ru</title>
+<title>
+<?php echo $my_title;?>
+</title>
 <meta charset="utf-8">
-<meta http-equiv="x-ua-compatible" content="IE=Edge"/>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="x-ua-compatible" content="IE=Edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" /> 
+<meta name="Description" content="<?php echo $description;?>" />
+<meta name="Keywords" content="моментальный заем, займы, онлайн, деньги" />
+<meta name="robots" content="all" />
+<meta name="copyright" lang="ru" content="Godzaim.ru" /> 
+
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="Cache-control" content="no-cache"> 
+<meta http-equiv="Expires" content="Wed, 22 Mar 2018 00:20:05 GMT">
+
+<meta property="og:title" content="Срочные займы круглосуточно без проверок Онлайн" />
+<meta property="og:description" content="Godzaim - лучший онлайн сервис по выдаче мгновенных займов и кредитов без проверки вашей кредитной истории.Только у нас лучшие кредитные предложения!"/>
+<meta property="og:image" content="https://godzaim.ru/templates/godzaim/img/how-bgg.png"/>
+<meta property="og:type" content="website" />
+<meta property="og:url" content="https://godzaim.ru/" />
+
+<meta name="twitter:card" content="https://godzaim.ru/templates/godzaim/img/how-bgg.png"/>
+<meta name="twitter:title" content="Срочные займы круглосуточно без проверок Онлайн"/>
+<meta name="twitter:description" content="Godzaim - лучший онлайн сервис по выдаче мгновенных займов и кредитов без проверки вашей кредитной истории.Только у нас лучшие кредитные предложения!"/>
+<meta name="twitter:image:src" content="https://godzaim.ru/templates/godzaim/img/how-bgg.png"/>
+<meta name="twitter:url" content="https://godzaim.ru/"/>
+<meta name="twitter:domain" content="godzaim.ru"/>
 
 <link rel="shortcut icon" href="/templates/godzaim/img/favicon.ico" type="image/x-icon">
 
-<link href="/templates/godzaim/css/ion.rangeSlider.css" rel="stylesheet" media="screen">
-<link href="/templates/godzaim/css/ion.rangeSlider.skinHTML5.css" rel="stylesheet" media="screen">
+<?php 
+		// CSS
+		echo '<style>';
+		require "templates/godzaim/css/ion.rangeSlider.css"; 
+		echo '</style>'; 
+		echo '<style>'; 
+		require "templates/godzaim/css/ion.rangeSlider.skinHTML5.css"; 
+		echo '</style>'; 
+		echo '<style>'; 
+		require "templates/godzaim/css/bootstrap.min.css"; 
+		echo '</style>'; 
+		echo '<style>'; 
+		require "templates/godzaim/css/font-awesome.min.css"; 
+		echo '</style>'; 
+		echo '<style>'; 
+		require "templates/godzaim/css/style.min.css"; 
+		echo '</style>'; 
+		echo '<style>
+		.row {
+			margin-right: 0px;  
+			margin-left: 0px;
+		}
+		</style>';
 
-<link href="/modules/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" media="screen">
-<link rel="stylesheet" type="text/css" href="/templates/godzaim/css/font-awesome.min.css">
+		//js
+		echo '<script>'; 
+		require "modules/jquery/jquery-1.11.3.min.js"; 
+		echo '</script>'; 
+	?>  
 
-<link href="/templates/godzaim/css/style.css?ver=2" rel="stylesheet" media="screen">
-<style>
-.row {
-    margin-right: 0px;  
-	margin-left: 0px;
-}
-</style>
-<script src="/modules/jquery/jquery-1.11.3.min.js"></script>
+<!-- <script src="/modules/jquery/jquery-1.11.3.min.js"></script> -->
 <?php if ($this->uri->segment(1) != 'form') {
     require 'templates/common/googlesyndication.php';
 }?>
@@ -30,33 +77,11 @@
 <![endif]-->
 
 <script type="text/javascript">(window.Image ? (new Image()) : document.createElement('img')).src = location.protocol + '//vk.com/rtrg?r=AB08QuA*AHqYGgS1qSBS6k8osS6uuM0vwS7*GJOxHTjgB6b3yrRzsaxRA1KdhNf2pLUkYjuXeJvfaRnExxKnHxYFNvF2NIeOXGQG6ID7zAK1LjO2*5JwKmrAYlejenz3cv*G0tu9UfpsE3Bj9StLFGJC4Xc7FnTUd7KiOjzBcJY-&pixel_id=1000081266';</script>
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WQ8TJJ9');</script>
-<!-- End Google Tag Manager -->
-<script>
- (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
- (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
- m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
- })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
- ga('create', 'UA-45591397-16', 'auto', {'name': 'vteleport_ru', 'allowLinker':true});
- ga('vteleport_ru.require', 'linker');
- ga('vteleport_ru.linker:autoLink',['onezaim.com','onezaim.ru','manimo.su','manimo.ru','my-zaim.com','my-zaim.ru','upfinance.ru','ezaimo.net','ezaimo.ru','tratimo.ru','vivuso.ru','vspz.ru','zaim365.ru','zaimmigom.ru','optima-zaim.ru', 'safezaim.com', 'zaimsrazu.ru', 'zanimalov.ru', 'getzaim.su', 'gutfin.ru', 'zanimalo.ru', 'kredito24.su', 'kredito24-mail.ru', 'zaimo-mail.ru', 'zaimomail.ru', 'mail-kredito.ru', 'kreditomail.ru', 'mail-zaimo.ru', 'dengimo.ru', 'dengoman.ru', 'edenga.ru', ' rublimo.ru', 'vkredito.ru', 'bzaim5.ru', 'dengibystra.ru']);
-
- ga(function() {
-   var gaTracker = ga.getByName('vteleport_ru');
-   var clientId = gaTracker.get('clientId');
-   ga('vteleport_ru.set', 'dimension1', clientId);
- });
-
- ga('vteleport_ru.send', 'pageview');
-
-</script>
-<script charset="UTF-8" src="//cdn.sendpulse.com/28edd3380a1c17cf65b137fe96516659/js/push/7967dc55391279be96419b0369cade45_1.js" async></script>
+<?php
+		require 'sendpuls.php';
+		require 'google_tag_manager.php';
+		require 'google_analytics.php';
+?> 
 
 </head>
 <body>
@@ -70,7 +95,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <nav class="navbar">
 	<div class="container">
 		<div class="navbar-header">
-			<a class="navbar-brand" href="/"><img alt="" src="/templates/godzaim/img/logo.png"></a>
+			<a class="navbar-brand" href="/"><img alt="logo.png" src="/templates/godzaim/img/logo.png"></a>
 			<button type="button" class="navbar-toggle collapsed pull-right" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
 				<span class="sr-only">Меню</span>
 				<i class="glyphicon glyphicon-menu-hamburger"></i>
