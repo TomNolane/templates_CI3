eae0a2775ad587c968ccbd3612bf113950b5b786
Tom Nolane <gotopredestenacia@gmail.com>
2017-12-15 10:37:28 +0300

    fix changes zaimol, bzaim, zaimsoon

--- bzaim/form2.php
+++ bzaim/form2.php
@@ -5,26 +5,22 @@
     <span id="passportstatus" class="glyphicon form-control-feedback" aria-hidden="true"></span>
 </div>
 
-<div class="form-group hidden">
-	<label class="col-sm-4 control-label label-required hidden-xs hidden-sm">Серия и номер паспорта</label>
-	<div class="col-sm-4">
-		<input type="tel" class="form-control ec" id="passport-s" name="passport_s" placeholder="0000" title="Серия паспорта" data-validation="number" data-validation-allowing="range[1;9999]" data-validation-error-msg="Пожалуйста, введите серию паспорта" required>
-	</div>
-	<div class="col-sm-4">
-		<input type="tel" class="form-control ec" id="passport-n" name="passport_n" placeholder="000000" title="Номер паспорта"  data-validation="number" data-validation-allowing="range[1;999999]" data-validation-error-msg="Пожалуйста, введите номер паспорта" required>
-	</div>
-</div>
+
+
 <div class="form-group has-feedback">
     <label class="control-label label-required hidden-xs hidden-sm">Дата выдачи паспорта</label>
     <input type="text" class="form-control ec tip" name="passportdate" id="passportdate" placeholder="Дата выдачи паспорта" title="Пожалуйста, выберете дату выдачи паспорта" data-validation="custom" data-validation-regexp="^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$" data-validation-error-msg="Пожалуйста, выберите дату выдачи паспорта" required>
     <span id="passportdatestatus" class="glyphicon form-control-feedback" aria-hidden="true"></span>
 </div>
+
+<input type="hidden" class="form-control ec" id="passport-s" name="passport_s" placeholder="0000" title="Серия паспорта" data-validation="number" data-validation-allowing="range[1;9999]" data-validation-error-msg="Пожалуйста, введите серию паспорта" required>
+<input type="hidden" class="form-control ec" id="passport-n" name="passport_n" placeholder="000000" title="Номер паспорта"  data-validation="number" data-validation-allowing="range[1;999999]" data-validation-error-msg="Пожалуйста, введите номер паспорта" required>
 <div class="form-group hidden">
 	<label class="col-sm-4 control-label label-required hidden-xs hidden-sm">Дата выдачи*</label>
 	<div class="col-sm-2">
 			<select class="form-control ec" id="passport_dd" name="passport_dd" required>
 				<option value="0">День</option>
-                                <option selected value="04"></option>
+				<option selected value="04"></option>
 				<?php
 				for($i=1;$i<=31;$i++)
 				echo '<option value="'.(($i<10)? '0' : '').$i.'">'.$i.'</option>';
