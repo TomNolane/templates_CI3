3f2222255454e10eddadf16d7be16344ac1769df
Tom Nolane <gotopredestenacia@gmail.com>
2017-12-28 18:35:05 +0300

    add small change dengoman.ru

--- dengoman/footer.php
+++ dengoman/footer.php
@@ -53,39 +53,7 @@ if($this->uri->segment(1) != 'form')
 </footer>
 
 <div class="clearfix"></div>
-<!-- Modal -->
-<div id="askQuestion" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header text-center">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                <h3 id="spec_modal">
-                    Заполните форму <br> и наш менеджер свяжется с вами
-                </h3>
-            </div>
-            <div class="modal-body">
-                <div class="form-group has-feedback">
-                    <input type="text" class="form-control ec tip" name="feedback-name" title="Пожалуйста, введите свое имя"
-                    data-sanitize="capitalize" data-validation="custom" data-validation-regexp="^[А-Яа-яЁё\-\s]+$"
-                    data-validation-error-msg="Пожалуйста, введите свое имя" id="feedback-name" placeholder="Введите ваше имя">
-                </div>
-                <div class="form-group">
-                    <input type="tel" class="form-control" id="feedback-phone" title="Тефелон" placeholder="Введите ваш телефон" required>
-                </div>
-                <div class="form-group">
-                    <input type="email" class="form-control" id="feedback-email" title="Email" placeholder="Введите ваш email" required>
-                </div>
-                <div class="form-group">
-                    <textarea id="feedback-comment" rows="5" class="form-control" title="Текст вашего сообщения" placeholder="Введите текст сообщения" data-sanitize="capitalize" data-validation="custom" data-validation-regexp="^[А-Яа-яЁё\d\s\-\,\#\.\+\(\)]+$" data-validation-error-msg="Введите текст сообщения" required></textarea>
-                </div>
-                <div class="text-center"><button type="button" id="feedback-send" class="btn">Отправить</button></div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- /Modal -->
+
 
 <?php 
     require 'templates/common/get_display_size.php';
